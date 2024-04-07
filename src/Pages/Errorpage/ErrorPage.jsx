8e31744f1a24b4bbf4f59dd74c3997e45a54cd81
Sakib <ssar96540@gmail.com>
2024-04-08 05:03:07 +0600

    Created a 404 page/ not found page

--- src/Pages/Errorpage/ErrorPage.jsx
+++ src/Pages/Errorpage/ErrorPage.jsx
@@ -1,8 +1,15 @@
+import { Link } from "react-router-dom";
+
  
 const ErrorPage = () => {
   return (
     <div>
-      <h1>error</h1>
+     <div className="flex justify-center flex-col min-h-screen items-center">
+        <h1 className="text-6xl playfair">440</h1> 
+        <h1 className="mt-3 text-6xl playfair">Page is not fount </h1>
+    <Link to='/'>
+    <button className="btn  bg-[#23BE0A] w-40 mt-4 text-lg font-semibold text-white">Go  Home</button></Link>
+      </div>
     </div>
   );
 };
