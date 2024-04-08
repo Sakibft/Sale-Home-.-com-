f6eeb3695b3b61f9546eb41862115e9c729d2012
Sakib <ssar96540@gmail.com>
2024-04-08 08:05:54 +0600

    completed navbar active routing style

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,13 +1,13 @@
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
  
 
 const Nav = () => {
   const n = 'Sakib Sarker'
   const navLinks = <> 
-  <li><Link to='/'>Home</Link></li>
-  <li><Link to='/update'>Update Profile</Link></li>
-  <li><Link to='/extra'>Extra</Link></li>
+  <NavLink className={({isActive})=>isActive? 'text-[#8EA7FF]  border px-2 py-1 border-[#8EA7FF] rounded-md   font-semibold text-xl  ':'font-semibold text-xl  '}  to='/'>Home</NavLink >
+  <NavLink   className={({isActive})=>isActive? 'text-[#8EA7FF]  border px-2 py-1 border-[#8EA7FF] rounded-md   font-semibold text-xl  ':'font-semibold text-xl  '}   to='/update'>Update Profile</NavLink >
+  <NavLink  className={({isActive})=>isActive? 'text-[#8EA7FF]  border px-2 py-1 border-[#8EA7FF] rounded-md   font-semibold text-xl  ':'font-semibold text-xl  '}    to='/extra'>Extra</NavLink >
    
   </>
   return (
@@ -17,7 +17,7 @@ const Nav = () => {
         <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
           <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
         </div>
-        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2   rounded-box w-52">
           {navLinks}
         </ul>
       </div>
