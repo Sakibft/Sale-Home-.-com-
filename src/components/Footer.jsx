4cd6d28ffba6deadf617c4a4206e52db15661b54
Sakib <ssar96540@gmail.com>
2024-04-15 22:51:25 +0600

    added a new look new footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,47 +1,85 @@
+import { BsDot } from "react-icons/bs";
 import { IoHome } from "react-icons/io5";
+import ksd from "../assets/undraw_cabin_hkfr.svg";
+import { FaFacebook, FaInstagram, FaYoutube } from "react-icons/fa";
+import { FaArrowRightFromBracket } from "react-icons/fa6";
+import { LiaPhoneSolid } from "react-icons/lia";
+import { HiOutlineMail } from "react-icons/hi";
+import { SlLocationPin } from "react-icons/sl";
 
 const Footer = () => {
   return (
     <div>
-      
       <footer className="flex flex-col text-black">
-            <div className="flex flex-col items-center justify-around gap-5 bg-blue-50 py-8 md:flex-row md:gap-0">
-                <aside className="flex items-center justify-center gap-3 text-xl">
-                <IoHome className="text-6xl"/>
+        <div className="flex flex-col items-center justify-around gap-5 bg-gray-100  md:flex-row md:gap-0">
+          <aside className="flex items-center justify-center gap-3 text-xl mt-3 ">
+           
+            <img className="w-44 mb-2" src={ksd} alt="" />
+           
+          </aside>
+          <div>
+            <h1 className="text-3xl text-center">Our Services</h1>
+            <ul className="flex flex-col justify-center items-center">
+              <li className="flex text-[#8EA7E9] underline">
+                <a href="">Legal Guidance</a>
+              </li>
+              <li className="flex text-[#8EA7E9] underline">
+                <a href="">Market Analysis</a>
+              </li>
 
-                    <p className="text-3xl mt-2">Home Sale</p>
-                </aside>
-                <nav className="text-lg">
-                    <ul className=" flex h-full items-center justify-center gap-5">
-                        <li className="cursor-pointer">
-                            <a>
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current">
-                                    <path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path>
-                                </svg>
-                            </a>
-                        </li>
-                        <li className="cursor-pointer">
-                            <a>
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current">
-                                    <path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path>
-                                </svg>
-                            </a>
-                        </li>
-                        <li className="cursor-pointer">
-                            <a>
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current">
-                                    <path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path>
-                                </svg>
-                            </a>
-                        </li>
-                    </ul>
-                </nav>
+              <li className="flex text-[#8EA7E9] underline">
+                <a href="">Property Valuation</a>
+              </li>
+              <li className="flex text-[#8EA7E9] underline">
+                <a href="">Investment Analysis</a>
+              </li>
+              <li className="flex text-[#8EA7E9] underline">
+                <a href="">Property Management</a>
+              </li>
+              <li className="flex text-[#8EA7E9] underline">
+                <a href="">Portfolio Diversification</a>
+              </li>
+            </ul>
+          </div>
+          <nav className="text-lg">
+            <ul className=" flex h-full items-center justify-center gap-5 ">
+              <li className="cursor-pointer">
+                <a className="flex gap-1">
+               <h1 className="text-[20px]">Follow Us ~</h1>
+               <FaArrowRightFromBracket className="mt-1 text-xl text-[#8EA7E9]" />
+
+                </a>
+              </li>
+              <li className="cursor-pointer">
+                <a>
+                <FaFacebook className="text-[#8EA7E9] text-4xl"/>
+
+                </a>
+              </li>
+              <li className="cursor-pointer">
+                <a>
+                <FaInstagram className="text-4xl"/>
+
+                </a>
+              </li>
+              <li className="cursor-pointer">
+                <a>
+                <FaYoutube  className="text-[#8EA7E9] text-4xl" />
+
+                </a>
+              </li>
+            </ul>
+            <div className="mt-2">
+            <h1 className="flex gap-2 ">Number : 01740228022 <LiaPhoneSolid className="mt-1" /> </h1>
+            <h1 className="flex gap-2">email : ssar96540@gmail.com <HiOutlineMail className="mt-2" /> </h1>
+            <h1 className="flex gap-2">location : Dhaka,Narshingdi,Shibpur <SlLocationPin className="mt-1" /></h1>
             </div>
-            <aside className="bg-sky-100 py-5 text-center text-sm">
-                <p>&copy; 2024 Sale ~ home. All Rights Reserved.</p>
-            </aside>
-        </footer>
-   
+          </nav>
+        </div>
+        <aside className="bg-[#8EA7E9] py-5 text-center text-sm">
+          <p className="text-white">&copy; 2024 Sale ~ home. All Rights Reserved.</p>
+        </aside>
+      </footer>
     </div>
   );
 };
