b6a68169e440838d6a3d953cb94db9e9085bb897
Sakib <ssar96540@gmail.com>
2024-04-16 22:59:44 +0600

    Changes error page >> added a 404 image in error pages

--- src/Pages/Errorpage/ErrorPage.jsx
+++ src/Pages/Errorpage/ErrorPage.jsx
@@ -1,14 +1,15 @@
 import { Link } from "react-router-dom";
-
+import oidk from "../../assets/asdod.svg"
  
 const ErrorPage = () => {
   return (
     <div>
      <div className="flex justify-center flex-col min-h-screen items-center">
-        <h1 className="text-6xl playfair">440</h1> 
-        <h1 className="mt-3 text-6xl playfair">Page is not fount </h1>
+ 
+        <img src={oidk} alt="" />
+        <h1 className="text-sm mt-1 text-gray-400">Page is not found</h1>
     <Link to='/'>
-    <button className="btn  bg-[#8EA7FF]  w-40 mt-4 text-lg font-semibold text-white">Go  Home</button></Link>
+    <button className="btn  bg-[#8EA7FF]  w-40 mt-1 text-lg font-semibold text-white">Go  Home</button></Link>
       </div>
     </div>
   );
