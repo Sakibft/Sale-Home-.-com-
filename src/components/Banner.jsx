9f2c7d66d0d0f668df26d4b7a2fb2b253f07972b
Sakib <ssar96540@gmail.com>
2024-04-17 07:50:12 +0600

    added 2 extra slide in banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,50 +1,50 @@
- 
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/pagination';
-import 'swiper/css/navigation';
-import '../../src/App.css'
- 
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import "../../src/App.css";
 
 // import required modules
-import { Pagination, Navigation, Autoplay } from 'swiper/modules';
+import { Pagination, Navigation, Autoplay } from "swiper/modules";
 
 const Banner = () => {
   return (
-    <div data-aos="zoom-in" data-aos-duration="1000" className='container mx-auto mt-8'>
-     
-       <Swiper
+    <div
+      data-aos="zoom-in"
+      data-aos-duration="1000"
+      className="container mx-auto mt-8"
+    >
+      <Swiper
         slidesPerView={1}
         spaceBetween={30}
         loop={true}
         pagination={{
           clickable: true,
         }}
-        autoplay={
-          {delay:3000}
-        }
+        autoplay={{ delay: 3000 }}
         navigation={true}
-        modules={[Navigation,Autoplay,Pagination]}
+        modules={[Navigation, Autoplay, Pagination]}
         className="mySwiper"
       >
         <SwiperSlide>
-        <div className='slide slide1'>
-      </div>
+          <div className="slide slide1"></div>
         </SwiperSlide>
-        <SwiperSlide> <div className='slide slide2'>
-      </div></SwiperSlide>
         <SwiperSlide>
-        <div className='slide slide3'>
-      </div>
+          <div className="slide slide2"></div>
         </SwiperSlide>
-        
+        <SwiperSlide>
+          <div className="slide slide3"></div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="slide slide4"></div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="slide slide5"></div>
+        </SwiperSlide>
       </Swiper>
-     
-
     </div>
-    
   );
 };
 
